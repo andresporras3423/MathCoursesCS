5079e69470e6c0fa62a139540ff4353e9f36de67
andresporras3423 <oscarrussi@outlook.com>
2023-06-15 15:42:57 -0500

    add second problem in precalculus

---
+++ MathCoursesCS/Business/GlobalFunctionBusiness.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Extensions.Options;
+using System.Collections.Generic;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+
+namespace MathCoursesCS.Business
+{
+    public class GlobalFunctionBusiness
+    {
+        public GlobalFunctionBusiness()
+        {
+
+        }
+
+        public string addPlus(int number)
+        {
+            if (number >= 0)
+            {
+                return "+" + Convert.ToString(number);
+            }
+            else
+            {
+                return Convert.ToString(number);
+            }
+        }
+
+        public string addPlus(double number)
+        {
+            if (number >= 0)
+            {
+                return "+" + Convert.ToString(number);
+            }
+            else
+            {
+                return Convert.ToString(number);
+            }
+        }
+
+        public string jsonResponse(String question, String solution, List<String> options)
+        {
+            Dictionary<String, Object> questionInfo = new Dictionary<String, Object>();
+            questionInfo["question"] = question;
+            questionInfo["solution"] = solution;
+            questionInfo["options"] = options;
+            //return Ok(JsonSerializer.Serialize("4+ln(5x)"));
+            JsonSerializerOptions jsonOptions = new JsonSerializerOptions
+            {
+                Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+                WriteIndented = true
+            };
+
+            string json = JsonSerializer.Serialize(questionInfo, jsonOptions);
+            return json;
+        }
+
+        public string jsonResponse(String question, int solution, List<String> options)
+        {
+            String strSolution = Convert.ToString(solution);
+            return jsonResponse( question, strSolution, options);
+        }
+
+        public string jsonResponse(String question, String solution, List<int> options)
+        {
+            List<string> strOptions = options.Select(opt => Convert.ToString(opt)).ToList();
+            return jsonResponse(question, solution, strOptions);
+        }
+
+        public string jsonResponse(String question, int solution, List<int> options)
+        {
+            String strSolution = Convert.ToString(solution);
+            List<string> strOptions = options.Select(opt => Convert.ToString(opt)).ToList();
+            return jsonResponse(question, strSolution, strOptions);
+        }
+    }
+}
--- MathCoursesCS/Controllers/PreCalculusController.cs
+++ MathCoursesCS/Controllers/PreCalculusController.cs
@@ -1,7 +1,9 @@
 ﻿using MathCoursesCS.Business;
 using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.SwaggerUI;
 using System;
 using System.Collections.Generic;
+using System.Text.Encodings.Web;
 using System.Text.Json;
 
 namespace MathCoursesCS.Controllers
@@ -10,14 +12,20 @@ namespace MathCoursesCS.Controllers
     public class PrecalculusController : Controller
     {
         PreCalculusBusiness pb = new PreCalculusBusiness();
+        GlobalFunctionBusiness gf = new GlobalFunctionBusiness();
 
-
+        // this challenge generate a series, this series has 5 values and the solution is the next value in the series
+        // the series are in either of the next ways:
+        // 1) a+bx
+        // 2) a+b^x
+        // 3) a+x^|b|
+        // the options are five continuous numbers where one of them include the solution
         [HttpGet]
         [Route("series1")]
-        public IActionResult Get()
+        public IActionResult series1()
         {
-            int addNumber = new Random().Next(11) - 5;
-            int opNumber = new Random().Next(11) - 5;
+            int addNumber = (new Random().Next(6) +1) * ((new Random().Next(2)*2)-1);
+            int opNumber = (new Random().Next(6) + 1) * ((new Random().Next(2) * 2) - 1);
             int seriesType = new Random().Next(3);
             List<int> series = new List<int>();
             if (seriesType==0)
@@ -35,13 +43,53 @@ namespace MathCoursesCS.Controllers
             string question = "Which is the next number in the series: " + String.Join(", ",series.GetRange(0, 5));
             int solution = series.Last();
             List<int> options = pb.getContinuousOptions(solution);
-            Dictionary<String, Object> questionInfo = new Dictionary<String, Object>();
-            questionInfo["question"] = question;
-            questionInfo["solution"] = solution;
-            questionInfo["options"] = options;
-            return Ok(JsonSerializer.Serialize(questionInfo));
+            return Content(gf.jsonResponse(question, solution, options), "application/json");
         }
 
+
+        [HttpGet]
+        [Route("type_series_problem")]
+        public IActionResult typeSeriesProblem()
+        {
+            int addNumber = (new Random().Next(6) + 1) * ((new Random().Next(2) * 2) - 1);
+            int opNumber = (new Random().Next(6) + 1) * ((new Random().Next(2) * 2) - 1);
+            int limitType = new Random().Next(6);
+            string func = "";
+            string solution = "";
+            if (limitType == 0)
+            {
+                func = Convert.ToString(addNumber)+ gf.addPlus(opNumber)+"x";
+                solution = opNumber > 0 ? "increasing" : "decreasing";
+            }
+            else if (limitType == 1)
+            {
+                func = Convert.ToString(addNumber) + "+("+Convert.ToString(opNumber) + "^x)";
+                solution = opNumber > 0 ? "increasing" : "alternating";
+            }
+            else if (limitType == 2)
+            {
+                func = Convert.ToString(addNumber) + "+x^(" + Convert.ToString(opNumber) + ")";
+                solution = opNumber > 0 ? "increasing" : "decreasing";
+            }
+            else if (limitType == 3)
+            { 
+                func = Convert.ToString(addNumber) + "+ln(" + Convert.ToString(opNumber) + "x)";
+                solution = opNumber > 0 ? "increasing" : "undefined";
+            }
+            else if (limitType == 4)
+            {
+                func = Convert.ToString(addNumber) + "+ln((" + Convert.ToString(opNumber) + ")^x)";
+                solution = opNumber > 0 ? "increasing" : "undefined";
+            }
+            else
+            {
+                func = Convert.ToString(addNumber) + "+ln(x^(" + Convert.ToString(opNumber) + "))";
+                solution = opNumber > 0 ? "increasing" : "decreased";
+            }
+            string question = "for the next function:"+func+" define among the options which one describe it better when x->∞";
+            List<string> options = new List<string> { "increasing", "decreasing", "alterning", "undefined" };
+            return Content(gf.jsonResponse(question, solution, options), "application/json");
+        }
 
     }
 }
